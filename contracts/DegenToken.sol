af4c5e59f8f48637c69f5f24dc5238d9a6e4aa37
Samyyak <108656939+Samyyak@users.noreply.github.com>
2024-07-07 04:04:36 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -10,6 +10,8 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
     event TokensRedeemed(address indexed redeemer, uint256 amount, string reward);
     event LoyaltyBadgeEarned(address indexed player, uint256 amount, string badge);
 
+    mapping(address => string[]) public inventory;
+
     constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) public onlyOwner {
@@ -28,34 +30,53 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
     function redeemTokens(address _redeemer, uint256 _amount) external {
         require(balanceOf(_redeemer) >= _amount, "The specified address does not have enough Degen Tokens to redeem this reward");
 
+        string memory reward;
         if (_amount == 300) {
-            _burn(_redeemer, _amount);
-            emit TokensRedeemed(_redeemer, _amount, "Degen Gradient Box");
+            reward = "Degen Gradient Box";
         } else if (_amount == 200) {
-            _burn(_redeemer, _amount);
-            emit TokensRedeemed(_redeemer, _amount, "Degen Legendary Box");
+            reward = "Degen Legendary Box";
         } else if (_amount == 100) {
-            _burn(_redeemer, _amount);
-            emit TokensRedeemed(_redeemer, _amount, "Degen Elite Box");
+            reward = "Degen Elite Box";
         } else {
             revert("Not enough tokens for any reward");
         }
+
+        _burn(_redeemer, _amount);
+        inventory[_redeemer].push(reward);
+        emit TokensRedeemed(_redeemer, _amount, reward);
     }
 
     function earnLoyaltyBadge(address _player, uint256 _amount) external {
         require(balanceOf(_player) >= _amount, "The specified address does not have enough Degen Tokens to earn this badge");
 
-        if (_amount >= 80) {
-            _burn(_player, _amount);
-            emit LoyaltyBadgeEarned(_player, _amount, "Degen Gradient Badge");
-        } else if (_amount >= 60) {
-            _burn(_player, _amount);
-            emit LoyaltyBadgeEarned(_player, _amount, "Degen Legendary Badge");
-        } else if (_amount >= 20) {
-            _burn(_player, _amount);
-            emit LoyaltyBadgeEarned(_player, _amount, "Degen Elite Badge");
+        string memory badge;
+        if (_amount == 80) {
+            badge = "Degen Gradient Badge";
+        } else if (_amount == 60) {
+            badge = "Degen Legendary Badge";
+        } else if (_amount == 20) {
+            badge = "Degen Elite Badge";
         } else {
             revert("Not enough tokens for any badge");
         }
+
+        _burn(_player, _amount);
+        inventory[_player].push(badge);
+        emit LoyaltyBadgeEarned(_player, _amount, badge);
+    }
+
+    function getInventory(address _owner) external view returns (string[] memory) {
+        return inventory[_owner];
+    }
+
+    function redeemRules() external pure returns (string memory) {
+        return "Loyalty Badges:\n"
+               "20 DGN - Degen Elite Badge\n"
+               "60 DGN - Degen Legendary Badge\n"
+               "80 DGN - Degen Gradient Badge\n\n"
+               "Degen Boxes:\n"
+               "100 DGN - Degen Elite Box\n"
+               "200 DGN - Degen Legendary Box\n"
+               "300 DGN - Degen Gradient Box";
     }
 }
